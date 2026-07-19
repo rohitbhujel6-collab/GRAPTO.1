3069ca26dfe5065c8002c1332885fb9dbd3c49ba
rohitbhujel6-collab <rohitbhujel6@gmail.com>
2026-07-19 14:20:06 +0530

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,61 @@
+/* ==========================================================
+   GRAPTO
+   WEBSITE APPLICATION
+   Version : 1.0
+   ========================================================== */
 
+"use strict";
+
+/* ==========================================================
+   APPLICATION
+   ========================================================== */
+
+const GRAPTO = {
+
+    init() {
+
+        console.log("==================================");
+        console.log("GRAPTO Website Started");
+        console.log("==================================");
+
+        this.firebase();
+
+        this.events();
+
+    },
+
+    /* ====================================================== */
+
+    firebase() {
+
+        if (typeof window.db === "undefined") {
+
+            console.error("Firebase Database Not Connected");
+
+            return;
+
+        }
+
+        console.log("Firebase Database Connected");
+
+    },
+
+    /* ====================================================== */
+
+    events() {
+
+        console.log("Website Events Loaded");
+
+    }
+
+};
+
+/* ==========================================================
+   START APPLICATION
+   ========================================================== */
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    GRAPTO.init();
+
+});
